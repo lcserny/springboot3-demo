89b484c581cffb815b10ac0fe5e6c41e53b9a50b
Leonardo Cserny <leo.cserny@gmail.com>
2024-05-27 08:28:41 +0300

    adds prop

--- src/main/java/net/cserny/demo/DemoApplication.java
+++ src/main/java/net/cserny/demo/DemoApplication.java
@@ -1,13 +1,29 @@
 package net.cserny.demo;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.java.Log;
+
 @SpringBootApplication
-public class DemoApplication {
+@Log
+public class DemoApplication implements CommandLineRunner {;
+
+    @Value("${torrent.webui.baseUrl:hardcoded}")
+    private String baseUrl;
+
+    @Value("${spring.data.mongodb.database:hardcoded_db}")
+    private String dbName;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("baseUrl: " + baseUrl);
+        log.info("dbName: " + dbName);
+    }
 }
